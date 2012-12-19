dcdfadd55d8eea08c0f53b52aa4e7276ad351065
aipeople <aipeople0513@gmail.com>
2012-12-19 01:34:14 -0800

    Create DSAnimationLabel.h

---
+++ DSAnimationLabel.h
@@ -0,0 +1,25 @@
+//
+//  DSAnimationLabel.h
+//  Caloriemeter
+//
+//  Created by 蘇 on 12/12/19.
+//  Copyright (c) 2012年 蘇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DSAnimationLabel : UILabel{
+    
+    int frameNum;
+    NSTimer *animationTimer;
+}
+@property (nonatomic, readwrite) float animateSpeed;
+@property (nonatomic, strong) NSMutableArray *stringArray;
+
+- (DSAnimationLabel *)initWithFrame:(CGRect)frame andStrings:(NSArray *) strings;
+- (void)startAnimationFromIndex:(NSInteger)startIndex toIndex:(NSInteger)endIndex;
+- (void)startAnimation;
+- (void)stopAnimationWithString:(NSString *)string;
+- (void)stopAnimationWithIndex:(NSInteger)index;
+- (void)stopAnimation;
+@end
